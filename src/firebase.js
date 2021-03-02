1a0fb943bf71a290f80d2e07347e17faca9f0b3d
Tushar Langer <tushar.langer@gmail.com>
2021-03-02 23:31:33 +0530

    initial project setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB3-8ztMSqhc005vldSA6c0UtZ76acBSR0',
+  authDomain: 'tiktok-clone-dc105.firebaseapp.com',
+  projectId: 'tiktok-clone-dc105',
+  storageBucket: 'tiktok-clone-dc105.appspot.com',
+  messagingSenderId: '844578084557',
+  appId: '1:844578084557:web:49b9ee692813ccb1e7bb84',
+  measurementId: 'G-6SJMEC9P0M',
+};
